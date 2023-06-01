ecfa452ac1d65fc049bd411cf46fff15e48f4e6c
Talman95 <taltro528@gmail.com>
2023-06-01 23:36:47 +0300

    added pagination for messages

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -8,14 +8,30 @@ export const messageController = {
     try {
       const chatId = req.params.chatId
 
+      if (!chatId) {
+        return res.status(400).json({ message: 'Ошибка' })
+      }
+
+      const page = Number(req.query.page || 1)
+      const limit = Number(req.query.limit || 10)
+
+      const skippedMessages = (page - 1) * limit
+
       const messages = await Message.find({ chat: chatId })
+        .skip(skippedMessages)
+        .limit(limit)
         .populate('sender', 'fullName avatarUrl email')
         .populate('chat')
 
-      res.json(messages)
+      const count = await Message.find({ chat: chatId }).countDocuments()
+
+      res.status(200).json({
+        messages,
+        totalCount: count,
+      })
     } catch (e) {
       res.status(400)
-      res.send('Неизвестная ошибка')
+      res.send(e)
     }
   },
   sendMessage: async (req: Request, res: Response) => {
